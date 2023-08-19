062b22031cb8ce7983fda9676f0bcb03ce8f3ae2
vickyrules <VICKYRULES1705@GMAIL.COM>
2023-08-19 12:03:25 +0530

    removed unnecessary files -stable

--- package-info.java
+++
@@ -1,6 +0,0 @@
-@EpoxyDataBindingPattern(rClass = R.class, layoutPrefix = "view_holder")
-package com.example.epoxyapplication.epoxyapplication;
-
-import com.airbnb.epoxy.EpoxyDataBindingPattern;
-import com.example.epoxyapplication.R;
-
